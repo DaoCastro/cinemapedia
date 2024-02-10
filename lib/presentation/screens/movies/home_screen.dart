b30af85213ac6b4d0b31628efbf59e9e5a3ab3e2
David Castro <dcastro@tribalworldwide.gt>
2024-02-09 22:52:03 -0600

    creation slideshow movies

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -33,15 +34,9 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movies = nowPlayingMovies[index];
-
-        return ListTile(
-          title: Text(movies.title),
-        );
-      },
+    final movieSlideshow = ref.watch(moviesSlideshowProvider);
+    return Column(
+      children: [const CustomAppbar(), MoviesSlideShow(movies: movieSlideshow)],
     );
   }
 }
